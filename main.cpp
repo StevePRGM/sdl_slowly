095098072da36b941dd1d04f1cbc606e7e54fa9b
Stephen B <StevePRGM@yahoo.com>
2021-02-28 18:07:10 -0600

    byron

--- main.cpp
+++ main.cpp
@@ -3,13 +3,35 @@
 #include <iostream>
 #include <string>
 #include <cmath>
+//#include <utility> // for const_cast<int>()
 #include "files/qBMP.cpp"
 #include "files/character.cpp"
 #include "files/SDLinterface.cpp"
 
 const int sWIDTH = 640;
 const int sHEIGHT = 480;
+const int sByron = 123;
 
+#define xyy_debug1 //SDL_GetMouseState( &mx, &my ); std::cout << "(" << mx << "," << my << ") ";
+#define xyy_debug2 //std::cout << (short)(e.button.button) << "_d(" << mx << "," << my << ") \n";
+#define xyy_debug3 //std::cout << (short)(e.button.button) << "_u(" << mx << "," << my << ") \n";
+#define xyy_debug4 //std::cout << "key: " << e.key.keysym.sym << "\n";
+#define xyy_debugX //some other debug code (currently commented out)
+#define xyy_debugX //some other debug code (currently commented out)
+#define xyy_debugX //some other debug code (currently commented out)
+#define xyy_debugX //some other debug code (currently commented out)
+
+/*
+draw(SDL_BlitSurface h1, SDL_Rect rs, SDL_BlitSurface h2, const SDL_Rect rd)
+{
+ //SDL_Rect r1 = {0,0,32,32};
+ //SDL_Rect r2 = {0,0,32,32};
+ //const SDL_Rect *rs = &r1;
+ //SDL_Rect *rd = &r2;
+
+ SDL_BlitSurface( h2, rs, h1, rd );
+}
+*/
 
 // ========================================================================
 // =========================    MAIN    ===================================
@@ -19,21 +41,16 @@ int main( int argc, char* args[] )
  if( SDL_Init( SDL_INIT_VIDEO ) < 0 ) { return 0; }
 
  int appFLAG=0;
-
  SDL_interface wind;
 
  qBMP xHi("images/hi.bmp");
  qBMP xAloha("images/aloha.bmp");
  qBMP xBlack("images/black.bmp");
 
- SDL_Rect r1 = {0,0,32,32};
- SDL_Rect r2 = {0,0,32,32};
- const SDL_Rect *rs = &r1;
- SDL_Rect *rd = &r2;
-
-
- character man1;
+ clsMover man1;
  man1.img = xHi.xSurface;
+ man1.imgbg = xBlack.xSurface;
+ man1.winSurface = wind.xSurface;
 
  unsigned int i=0;
 
@@ -42,70 +59,60 @@ int main( int argc, char* args[] )
  SDL_Event e;
  while(appFLAG==0)
  {
+  int mx; int my;
   while( SDL_PollEvent( &e ) != 0 )
   {
    //printf("= %i\n", e.type);
    if( e.type == SDL_QUIT )
    {
     appFLAG = 1;
+   } else if( e.type == SDL_MOUSEBUTTONDOWN ) {
+    xyy_debug2
+   } else if( e.type == SDL_MOUSEBUTTONUP ) {
+    xyy_debug3
+   } else if( e.type == SDL_MOUSEMOTION ) {
+    xyy_debug1
    } else if( e.type == SDL_KEYDOWN ) {
+    xyy_debug4
     switch( e.key.keysym.sym )
     {
-     //load
+     case SDLK_ESCAPE:
+      appFLAG=1;
+      break;
+     case SDLK_0:
+      break;
      case SDLK_1:
       printf(" key1 - x\n");
       break;
-
-     //display
      case SDLK_2:
       printf(" key2 - display \n");
-      SDL_BlitSurface( man1.img, NULL, wind.xSurface, NULL );
-      rd->x = (unsigned int)man1.x;
-      rd->y = 32;
-      SDL_BlitSurface( xHi.xSurface, rs, wind.xSurface, rd );
-      SDL_UpdateWindowSurface( wind.xWindow );
       break;
      case SDLK_3:
       printf(" key3 - display \n");
-      SDL_BlitSurface( xAloha.xSurface, NULL, wind.xSurface, NULL );
-      SDL_UpdateWindowSurface( wind.xWindow );
       break;
      case SDLK_4:
       printf(" key4 - display \n");
-      SDL_BlitSurface( xBlack.xSurface, NULL, wind.xSurface, NULL );
-      SDL_UpdateWindowSurface( wind.xWindow );
-      break;
-
-     //free and NULL
-     case SDLK_0:
-      break;
-
-     //quit
-     case SDLK_SLASH:
-      appFLAG=1;
       break;
 
      case SDLK_LEFT:
-       SDL_BlitSurface( xBlack.xSurface, NULL, wind.xSurface, rd );
-      man1.x-=5;
-       std::cout << "man1.x = " << man1.x << "\n";
-       rd->x = (unsigned int)man1.x;
-       rd->y = 32;
-       SDL_BlitSurface( xHi.xSurface, rs, wind.xSurface, rd );
+       std::cout << "man1@(" << man1.x << "," << man1.y << ")\n";
+       man1.tele(-5,0);
        SDL_UpdateWindowSurface( wind.xWindow );
       break;
      case SDLK_RIGHT:
-       SDL_BlitSurface( xBlack.xSurface, NULL, wind.xSurface, rd );
-      man1.x+=5;
-       std::cout << "man1.x = " << man1.x << "\n";
-       rd->x = (unsigned int)man1.x;
-       rd->y = 32;
-       SDL_BlitSurface( xHi.xSurface, rs, wind.xSurface, rd );
+       std::cout << "man1@(" << man1.x << "," << man1.y << ")\n";
+       man1.tele(+5,0);
        SDL_UpdateWindowSurface( wind.xWindow );
       break;
      case SDLK_UP:
+       std::cout << "man1@(" << man1.x << "," << man1.y << ")\n";
+       man1.tele(0,-5);
+       SDL_UpdateWindowSurface( wind.xWindow );
       break;
      case SDLK_DOWN:
+       std::cout << "man1@(" << man1.x << "," << man1.y << ")\n";
+       man1.tele(0,+5);
+       SDL_UpdateWindowSurface( wind.xWindow );
       break;
 
      default:
@@ -119,6 +126,16 @@ int main( int argc, char* args[] )
 }
 
 
+
+
+// should use a union...
+void fDEBUG(int i, int a1=0, int a2=0, int a3=0, int a4=0)
+{
+ if(i==1) {
+  std::cout << "(" << a1 << "," << a2 << ") ";
+ } else {
+ }
+}
 
 /*
 
